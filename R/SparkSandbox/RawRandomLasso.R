5953fe08973f1703e1eaacc4b9573038c20da287
James Matthew Hamilton <jamesmatthewhamilton@icloud.com>
2019-04-22 13:09:43 -0400

    Spark sandbox now randomly bootraps and runs lasso.

--- R/SparkSandbox/RawRandomLasso.R
+++ R/SparkSandbox/RawRandomLasso.R
@@ -7,25 +7,74 @@
   library(glmnet)
   library(sparklyr)
   library(dplyr)
+  # sessionInfo() # to view package versions
 
   spark_install()
   conf <- spark_config()
-  conf$`sparklyr.cores.local` <- 2
-  conf$`sparklyr.shell.driver-memory` <- "8G"
+  conf$sparklyr.cores.local <- 2
+  conf$sparklyr.shell.driver-memory <- "8G"
   conf$spark.memory.fraction <- 0.9
 
   sc <- spark_connect(master = "local", config = conf)
 
 
-  setwd("~/Dropbox/KSULasso/R/Spark/")
+  setwd("~/Dropbox/KSULasso/R/SparkSandbox/")
   x = read.csv("x", row.names = 1)
   y = read.csv("y", row.names = 1)
+  colnames(y) <- "dependent"
+  all <- cbind(y, x)
   alpha = c(0.5, 0.5)
   verbose = TRUE
   number.of.features <- ncol(x)
   number.of.samples <- nrow(x)
-  bootstraps<- round(number.of.features / number.of.samples) * 80
+  bootstraps <- round(number.of.features / number.of.samples) * 80
   cut.off <- 1 / number.of.samples
+  empty <- matrix(NA, ncol = number.of.features, nrow = bootstraps)
+
+  # x_sp <- copy_to(sc, x, overwrite = TRUE)
+  # y_sp <- copy_to(sc, y, overwrite = TRUE)
+  all <- copy_to(sc, all, overwrite = TRUE)
+  empty <- copy_to(sc, empty, overwrite = TRUE)
+
+
+# ------------ Playground ------------
+
+  x_sp %>% head()
+  x_sp %>% colnames()
+
+  partitions <- x_sp %>%
+    select(c(1,3,9))
+
+  partitions %>% head()
+
+  avgs <- summarize_all(x_sp, funs(mean)) %>% as.data.frame()
+  exprs <- as.list(paste(colnames(x_sp),"-", avgs))
+  x_sp %>%
+    spark_dataframe() %>%
+    invoke("selectExpr", exprs) %>%
+    invoke("toDF", as.list(colnames(x_sp))) %>%
+    invoke("registerTempTable", "centered")
+  xx <- tbl(sc, "centered")
+  xx %>% head()
+
+  random.features <- sample(number.of.features,
+                            number.of.samples,
+                            replace = FALSE)
+  random.all <- all %>%
+    select(c(1,random.features))
+
+  random.all %>% ml_linear_regression(response = dependent ~ .,
+                                             fit_intercept = TRUE,
+                                             lastic_net_param = 1)
+
+  iris_tbl <- sdf_copy_to(sc, iris, overwrite = TRUE)
+  iris_tbl %>% spark_apply(function(e){cv.glmnet(as.matrix(x), as.matrix(y),
+                                                 type.measure = "mse",
+                                                 nfold = 5, alpha = 1)$lambda})
+
+  all %>% nrow2()
+
+  # ------------ Playground END ------------
 
   # ------------ Step I ------------ #
 
@@ -34,11 +83,11 @@
     pb <- txtProgressBar(min = 0, max = bootstraps, style = 3)
   }
 
-  beta.hat <- lapply(seq_len(bootstraps), .helper.part.a, x, y,
+  beta.hat3 <- lapply(seq_len(2), .helper.part.a, all,
                      number.of.features, number.of.samples,
                      bootstraps, pb, as.numeric(Sys.time()), alpha[1], verbose)
 
-  importance.measure <- abs(Reduce('+', beta.hat))
+  importance.measure <- abs(Reduce('+', beta.hat3))
 
   # ------------ Step II ------------ #
 
@@ -70,14 +119,36 @@
 # >>>>>>>>>>>> FUNCTION PART A <<<<<<<<<<<<
 # [Description] Randomly samples features from x and runs Random Lasso.
 # [RETURNS] Coefficients from Random Lasso.
-.helper.part.a <- function(ii, x, y, number.of.features,
+.helper.part.a <- function(ii, all, number.of.features,
                            number.of.samples, bootstraps, pb,
                            start.time, alpha, verbose, importance) {
   beta.hat <- numeric(number.of.features)
   if (verbose) {.helper.time.remaining(pb, start.time, ii, bootstraps)}
 
   # Sample features column numbers equal to the number of samples.
+  random.features <- sample(number.of.features, number.of.samples,
+                            replace = FALSE)
+  #
+  random.all <- all %>%
+    select(c(1,random.features))
 
+  fit <- random.all %>% ml_linear_regression(response = dependent ~ .,
+                                             fit_intercept = TRUE,
+                                             lastic_net_param = 1)
+
+  return(fit)
+}
+
+# >>>>>>>>>>>> FUNCTION PART B <<<<<<<<<<<<
+# [Description] Randomly samples features WITH WEIGHT from x and runs Random Lasso.
+# [RETURNS] Coefficients from Random Lasso.
+.helper.part.b <- function(ii, x, y, number.of.features,
+                           number.of.samples, bootstraps, pb,
+                           start.time, alpha, verbose) {
+  beta.hat <- numeric(number.of.features)
+  if (verbose) {.helper.time.remaining(pb, start.time, ii, bootstraps)}
+
+  # Sample features column numbers equal to the number of samples.
   random.features <- sample(number.of.features, number.of.samples,
                             replace = FALSE)
   # Mix up the rows.
@@ -96,46 +167,13 @@
   random.x.scale <- scale(random.x, random.x.mean, FALSE)
   standard.deviation <- sqrt(apply(random.x.scale^2, 2, sum))
   random.x.scale <- scale(random.x.scale, FALSE, standard.deviation)
+
   # Filling in the empty rows one by one with results from lasso.
-  beta.hat[random.features] <- Lasso(random.x.scale,random.y.scale,
-                                     alpha)
+  beta.hat[random.features] <- Lasso(random.x.scale, random.y.scale, alpha) / standard.deviation
+
   return(beta.hat)
 }
 
-# >>>>>>>>>>>> FUNCTION PART B <<<<<<<<<<<<
-# [Description] Randomly samples features WITH WEIGHT from x and runs Random Lasso.
-# [RETURNS] Coefficients from Random Lasso.
-.helper.part.b <- function(ii, x, y, number.of.features,
-                           number.of.samples, bootstraps, pb,
-                           start.time, alpha, verbose, importance) {
-  beta.hat <- numeric(number.of.features)
-  if (verbose) {.helper.time.remaining(pb, start.time, ii, bootstraps)}
-
-  # Sample features column numbers equal to the number of samples.
-  random.features <- sample(number.of.features, number.of.samples,
-                            replace = FALSE, prob = importance)
-  # Mix up the rows.
-  random.samples <- sample(number.of.samples, replace = TRUE)
-  # Subset the columns and rows from the independent data.
-  random.x <- x[random.samples, random.features]
-  # Subset the rows from the dependent data.
-  random.y <- y[random.samples, ]
-
-  # Centering the dependent variable.
-  random.y.mean <- mean(random.y)
-  random.y.scale <- random.y - random.y.mean
-
-  # Centering the independent variable.
-  random.x.mean <- apply(random.x, 2, mean)
-  random.x.scale <- scale(random.x, random.x.mean, FALSE)
-  standard.deviation <- sqrt(apply(random.x.scale^2, 2, sum))
-  random.x.scale <- scale(random.x.scale, FALSE, standard.deviation)
-
-  # Filling in the empty rows one by one with results from lasso.
-  beta.hat[random.features] <- Lasso(random.x.scale,random.y.scale,
-                                     alpha) / standard.deviation
-  return(beta.hat)
-}
 
 # >>>>>>>>>>>> FUNCTION LASSO <<<<<<<<<<<<
 # [Description] Used in .helper.part.a and .helper.part.b
@@ -150,6 +188,7 @@ Lasso <- function(independent, dependent, alpha) {
   hat.beta <- coef(lasso.results)[-1]
   return(hat.beta)
 }
+
 
 # >>>>>>>>>>>> FUNCTION TIME HELPER <<<<<<<<<<<<
 # [Description] Controls printing of progress bar and time remaining.
@@ -168,6 +207,3 @@ Lasso <- function(independent, dependent, alpha) {
 
   cat("\r", paste("[",hr, ":", min, ":", sec, "] |", sep = ""))
 }
-
-
-
